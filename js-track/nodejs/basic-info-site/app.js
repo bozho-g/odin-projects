6d7acb365ff905051d137e85a483715a577f02f6
Bozhidar Garkov <goshevc@gmail.com>
2025-10-27 09:39:44 +0200

    Update basic info project to use express

---
+++ js-track/nodejs/basic-info-site/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
