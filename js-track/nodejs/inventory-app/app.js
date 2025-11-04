04fcc6203beb44ae7055cc36479ac996bef21d9b
Bozhidar Garkov <goshevc@gmail.com>
2025-11-04 21:06:12 +0200

    Add correct views pathname

--- js-track/nodejs/inventory-app/app.js
+++ js-track/nodejs/inventory-app/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mountRoutes = require('./routers');
 const { ValidationError } = require('express-validation');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 mountRoutes(app);
