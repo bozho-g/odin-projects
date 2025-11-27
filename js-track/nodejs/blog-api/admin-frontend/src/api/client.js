fb589ef4f26d0d6b6f5028c49ee013b79c0269df
Bozhidar Garkov <goshevc@gmail.com>
2025-11-27 22:35:35 +0200

    Update admin api base url

--- js-track/nodejs/blog-api/admin-frontend/src/api/client.js
+++ js-track/nodejs/blog-api/admin-frontend/src/api/client.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://odin-projects-production.up.railway.app/api';
 
 const api = async (endpoint, options = {}) => {
     const token = localStorage.getItem('accessToken');
